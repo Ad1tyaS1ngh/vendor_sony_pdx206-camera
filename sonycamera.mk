f1b95fdc409435e57db019c4628f1e9472d1e618
someone5678 <someone5678@users.noreply.github.com>
2023-11-17 13:21:26 +0900

    pdx206-camera: Build dummy frameworks
    Change-Id: Ib1a6ca443cc369364fa74caebe84bf212818f779

--- sonycamera.mk
+++ sonycamera.mk
@@ -1,3 +1,8 @@
+# Framework
+PRODUCT_PACKAGES += \
+	com.sonyericsson.idd_impl \
+	com.sony.device
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product.xml

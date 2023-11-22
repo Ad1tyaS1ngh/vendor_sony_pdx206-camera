4921701803c4a77c6e569a7ef6596d06292833a2
Aditya Singh <adityasingh1768@gmail.com>
2023-11-22 11:45:44 +0530

    pdx206-camera: Add google lens prop

--- sonycamera.mk
+++ sonycamera.mk
@@ -7,4 +7,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product.xml
 
+# Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.lens.oem_camera_package=com.sonymobile.photopro
+
 $(call inherit-product, vendor/sony/pdx206-camera/pdx206-camera-vendor.mk)
